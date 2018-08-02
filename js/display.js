1c3b1b15493b29ddf83ec8c57ee2229b80372442
Steelhead <steelheadWhatwhat@steelhead.com>
2018-08-01 20:51:06 -0700

    game so far

--- js/display.js
+++ js/display.js
@@ -1,3 +1,62 @@
 let gridSize = 40;
-let canvasWidth = COLS * gridSize;
-let canvasHeight = ROWS * gridSize;
+let gridMargin = 10;
+let canvasWidth = COLS * gridSize + 2 * gridMargin;
+let canvasHeight = ROWS * gridSize + 2 * gridMargin;
+
+let colors = [
+    "red",
+    "blue",
+    "orange",
+    "pink",
+    "green"
+];
+
+function translateXY(r,c){
+    let x = c * gridSize + gridSize / 2 + gridMargin;
+    let y = r * gridSize + gridSize / 2 + gridMargin;
+    return {
+        x:x, y:y
+    };
+}
+
+function drawPlayer(p){
+
+    stroke("white");
+    let w = Math.sin(frameCount / 3) * 4 + 3;
+    strokeWeight(w);
+    //let x = p.c * gridSize + gridSize / 2;
+    //let y = p.r * gridSize + gridSize / 2;
+
+    let coord = translateXY(p.r,p.c);
+    
+    fill(colors[p.colorIndex]);            
+    ellipse(coord.x,coord.y,gridSize /2);
+}
+function drawEnemy(p){
+
+    stroke("white");
+    let w = Math.sin(frameCount / 3) * 4 + 3;
+    strokeWeight(w);
+    //let x = p.c * gridSize + gridSize / 2;
+    //let y = p.r * gridSize + gridSize / 2;
+
+    let coord = translateXY(p.r,p.c);
+    
+    fill(colors[p.colorIndex]);            
+    ellipse(coord.x,coord.y,gridSize /2);
+}
+function drawBoard(){
+    noStroke();
+    // LOOP THROUGH THE BOARD
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+           // let x = c * gridSize + gridSize / 2 + gridMargin;
+           // let y = r * gridSize + gridSize / 2 + gridMargin;
+           let coord = translateXY(r,c);
+           let cellValue = game.board[r][c]; 
+            fill(colors[cellValue]);            
+            ellipse(coord.x,coord.y,gridSize /2);
+         
+    }
+}
+}
